177584cc1eabc0dee0558e8c0eefcf698a87e64c
n0beard <kotanr@icloud.com>
2020-02-14 17:46:14 -0600

    Add files via upload
    Added several more wordlists and implemented a better / faster method of downloading from hashes.org

--- holy_wow.sh
+++ holy_wow.sh
@@ -30,6 +30,11 @@
 
 # clear; while true; do clear; ls -lha; sleep
 
+
+# Counter for number of files downloaded
+#dlFiles=$((dlFiles+1))
+dlFiles=0
+
 # Create a function to display time elapsed
 timer () {
 if (( $SECONDS > 3600 )) ; then
@@ -44,6 +49,8 @@ elif (( $SECONDS > 60 )) ; then
 else
     echo "Timer: $SECONDS seconds"
 fi
+
+echo "Total files downloaded: $dlFiles"
 }
 
 
@@ -61,11 +68,13 @@ SECONDS=0
 
 
 
+
 ### Start with hashcat's example.dict on their githib
 ### https://github.com/hashcat/hashcat
-curl https://raw.githubusercontent.com/hashcat/hashcat/master/example.dict
+curl -O https://raw.githubusercontent.com/hashcat/hashcat/master/example.dict
 cat example.dict >> holy_wow.txt
 rm example.dict
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -77,13 +86,15 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/twitter-banned.txt
 cat twitter-banned.txt >> holy_wow.txt
 rm twitter-banned.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
-
+clear
 # probable-v2-top12000.txt
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/probable-v2-top12000.txt
 cat probable-v2-top12000.txt >> holy_wow.txt
 rm probable-v2-top12000.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -91,6 +102,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/openwall.net-all.txt
 cat openwall.net-all.txt >> holy_wow.txt
 rm openwall.net-all.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -98,6 +110,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/mssql-passwords-nansh0u-guardicore.txt
 cat mssql-passwords-nansh0u-guardicore.txt >> holy_wow.txt
 rm mssql-passwords-nansh0u-guardicore.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -105,6 +118,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/dutch_wordlist
 cat dutch_wordlist >> holy_wow.txt
 rm dutch_wordlist
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -112,6 +126,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/darkweb2017-top10000.txt
 cat darkweb2017-top10000.txt >> holy_wow.txt
 rm darkweb2017-top10000.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -119,6 +134,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/bt4-password.txt
 cat bt4-password.txt >> holy_wow.txt
 rm bt4-password.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -126,6 +142,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/PHP-Magic-Hashes.txt
 cat PHP-Magic-Hashes.txt >> holy_wow.txt
 rm PHP-Magic-Hashes.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -133,6 +150,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Most-Popular-Letter-Passes.txt
 cat Most-Popular-Letter-Passes.txt >> holy_wow.txt
 rm Most-Popular-Letter-Passes.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -140,6 +158,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Keyboard-Combinations.txt
 cat Keyboard-Combinations.txt >> holy_wow.txt
 rm Keyboard-Combinations.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -147,6 +166,7 @@ timer
 curl -O https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/xato-net-10-million-passwords.txt
 cat xato-net-10-million-passwords.txt >> holy_wow.txt
 rm xato-net-10-million-passwords.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -162,6 +182,7 @@ timer
 #rm Md5decrypt-awesome-wordlist.7z
 #cat Md5decrypt-awesome-wordlist.txt >> holy_wow.txt
 #rm Md5decrypt-awesome-wordlist.txt
+#dlFiles=$((dlFiles+1))
 #clear
 #timer
 
@@ -175,16 +196,18 @@ bzip2 -dk rockyou.txt.bz2
 rm rockyou.txt.bz2
 cat rockyou.txt >> holy_wow.txt
 rm rockyou.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
 
 # MySpace
-wget -O myspace.txt.bz2 --max-redirect=0 --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0" --keep-session-cookies --save-cookies cookies.txt --post-data --header="Host: somewebsite.com" --header="Referer: https://google.com" "http://downloads.skullsecurity.org/passwords/myspace.txt.bz2"
+wget -O myspace.txt.bz2 "http://downloads.skullsecurity.org/passwords/myspace.txt.bz2"
 bzip2 -dk myspace.txt.bz2
 rm myspace.txt.bz2
 cat myspace.txt >> holy_wow.txt
 rm myspace.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -194,6 +217,7 @@ wget -O crackstation.txt.gz "https://crackstation.net/files/crackstation.txt.gz"
 rm crackstation.txt.gz
 cat realuniq.lst >> holy_wow.txt
 rm realuniq.lst
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -203,6 +227,7 @@ bzip2 -dk john.txt.bz2
 rm john.txt.bz2
 cat john.txt >> holy_wow.txt
 rm john.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -212,6 +237,7 @@ bzip2 -dk cain.txt.bz2
 rm cain.txt.bz2
 cat cain.txt >> holy_wow.txt
 rm cain.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -221,6 +247,7 @@ bzip2 -dk conficker.txt.bz2
 rm conficker.txt.bz2
 cat conficker.txt >> holy_wow.txt
 rm conficker.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -230,6 +257,7 @@ bzip2 -dk phpbb.txt.bz2
 rm phpbb.txt.bz2
 cat phpbb.txt >> holy_wow.txt
 rm phpbb.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -239,6 +267,7 @@ bzip2 -dk hotmail.txt.bz2
 rm hotmail.txt.bz2
 cat hotmail.txt >> holy_wow.txt
 rm hotmail.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -248,6 +277,7 @@ bzip2 -dk faithwriters.txt.bz2
 rm faithwriters.txt.bz2
 cat faithwriters.txt >> holy_wow.txt
 rm faithwriters.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -257,6 +287,7 @@ bzip2 -dk elitehacker.txt.bz2
 rm elitehacker.txt.bz2
 cat elitehacker.txt >> holy_wow.txt
 rm elitehacker.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -266,6 +297,47 @@ bzip2 -dk hak5.txt.bz2
 rm hak5.txt.bz2
 cat hak5.txt >> holy_wow.txt
 rm hak5.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# DirBuster Big
+wget -O big.txt "https://raw.githubusercontent.com/digination/dirbuster-ng/master/wordlists/big.txt"
+cat big.txt >> holy_wow.txt
+rm big.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# DirBuster Euskera
+wget -O Euskera.txt "https://raw.githubusercontent.com/digination/dirbuster-ng/master/wordlists/euskera.txt"
+cat Euskera.txt >> holy_wow.txt
+rm Euskera.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# DirBuster Catala
+wget -O Catala.txt "https://github.com/digination/dirbuster-ng/blob/master/wordlists/catala.txt"
+cat Catala.txt >> holy_wow.txt
+rm Catala.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# SET FastTrack
+wget -O FastTrack.txt "https://raw.githubusercontent.com/trustedsec/social-engineer-toolkit/master/src/fasttrack/wordlist.txt"
+cat FastTrack.txt >> holy_wow.txt
+rm FastTrack.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# Fern Wifi Cracker list
+wget -O Fern.txt "https://raw.githubusercontent.com/savio-code/fern-wifi-cracker/master/Fern-Wifi-Cracker/extras/wordlists/common.txt"
+cat Fern.txt >> holy_wow.txt
+rm Fern.txt
+dlFiles=$((dlFiles+1))
 clear
 timer
 
@@ -273,38 +345,134 @@ timer
 
 
 
-### Download all from hashes.org
-### Get StopPoint from https://hashes.org/hashlists.php
+### wFuzz lists
+# test_ext
+wget -O test_ext.txt "https://raw.githubusercontent.com/xmendez/wfuzz/master/wordlist/stress/test_ext.txt"
+cat test_ext.txt >> holy_wow.txt
+rm test_ext.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# common_pass
+wget -O common_pass.txt "https://raw.githubusercontent.com/xmendez/wfuzz/master/wordlist/others/common_pass.txt"
+cat common_pass.txt >> holy_wow.txt
+rm common_pass.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# Names
+wget -O names.txt "https://raw.githubusercontent.com/xmendez/wfuzz/master/wordlist/others/names.txt"
+cat names.txt >> holy_wow.txt
+rm names.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# megabeast
+wget -O megabeast.txt "https://raw.githubusercontent.com/xmendez/wfuzz/master/wordlist/general/megabeast.txt"
+cat megabeast.txt >> holy_wow.txt
+rm megabeast.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# spanish
+wget -O spanish.txt "https://raw.githubusercontent.com/xmendez/wfuzz/master/wordlist/general/spanish.txt"
+cat spanish.txt >> holy_wow.txt
+rm spanish.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+
+
+
+### nmap lists
+# Passwords
+wget -O passwords.lst "https://raw.githubusercontent.com/nmap/nmap/master/nselib/data/passwords.lst"
+sed -i '/#!comment/d' ./passwords.lst   ### Remove comments
+cat passwords.lst >> holy_wow.txt
+rm passwords.lst
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# vHosts
+wget -O vhosts-full.lst "https://raw.githubusercontent.com/nmap/nmap/master/nselib/data/vhosts-full.lst"
+cat vhosts-full.lst >> holy_wow.txt
+rm vhosts-full.lst
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+
+
+
+### Hashes.org
+# 2012-2019 consolidated list
+wget -O hashes.org-2012-2019.7z --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0" --keep-session-cookies --save-cookies cookies.txt --post-data --header="Host: somewebsite.com" --header="Referer: https://hashes.org/left.php" "https://hashes.org/download.php?foundId=true&type=found&year=2012-2019"
+7za e hashes.org-2012-2019.7z
+rm hashes.org-2012-2019.7z
+cat hashes.org-2012-2019.txt >> holy_wow.txt
+rm hashes.org-2012-2019.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+# 2020 list
+wget -O hashes.org-2020.7z --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0" --keep-session-cookies --save-cookies cookies.txt --post-data --header="Host: somewebsite.com" --header="Referer: https://hashes.org/left.php" "https://hashes.org/download.php?foundId=true&type=found&year=2020"
+7za e hashes.org-2020.7z
+rm hashes.org-2020.7z
+cat hashes.org-2020.txt >> holy_wow.txt
+rm hashes.org-2020.txt
+dlFiles=$((dlFiles+1))
+clear
+timer
+
+
+
+### Download all from hashes.org https://hashes.org/left.php
+### https://hashes.org/download.php?foundId=true&type=found&year=2012-2019
+### https://hashes.org/download.php?foundId=true&type=found&year=2020
+
+
+
+### This here is to grab the hashes.org PWs the long and hard way. Created before I found the consolidated lists:
+### https://hashes.org/left.php
+### https://hashes.org/download.php?foundId=true&type=found&year=2012-2019
+### https://hashes.org/download.php?foundId=true&type=found&year=2020
 
 # Set the start point
-StartPoint=1
+# StartPoint=1
 
-# Get StopPoint from  5 and store as StopPoint
-rm hashlists.php
-curl -O https://hashes.org/hashlists.php
-read StopPoint <<< $(sed 's/[^0-9]//g' <<< $(grep -m 1 td hashlists.php))
-rm hashlists.php
+# Get StopPoint from https://hashes.org/hashlists.php and store as StopPoint
+# rm hashlists.php
+# curl -O https://hashes.org/hashlists.php
+# read StopPoint <<< $(sed 's/[^0-9]//g' <<< $(grep -m 1 td hashlists.php))
+# rm hashlists.php
 
 # Iterate from StartPoint to StopPoint and download all of the cracked lists
-for (( i=$StartPoint; i<=$StopPoint; i++ ))
-do
-   clear
-   timer
-   echo "Sleeping 1 second... (hashlistId = $i of $StopPoint)"
-   sleep 1
-   clear
-   timer
-   echo -e "We're on hashlistId $i of $StopPoint\n"
-   wget -O $i.txt --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0" --keep-session-cookies --save-cookies cookies.txt --post-data --header="Host: somewebsite.com" --header="Referer: https://hashes.org/leaks.php?id=$i" "https://hashes.org/download.php?type=found&hashlistId=$i"
+# for (( i=$StartPoint; i<=$StopPoint; i++ ))
+# do
+#    clear
+#    timer
+#    echo "Sleeping 1 second... (hashlistId = $i of $StopPoint)"
+#    sleep 1
+#    clear
+#    timer
+#    echo -e "We're on hashlistId $i of $StopPoint\n"
+#    wget -O $i.txt --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0" --keep-session-cookies --save-cookies cookies.txt --post-data --header="Host: somewebsite.com" --header="Referer: https://hashes.org/leaks.php?id=$i" "https://hashes.org/download.php?type=found&hashlistId=$i"
 
    ### Find all occurrances of "Invalid hashlist!" and "Hashlist is deleted!" from hashes.org lists and delete them
-   if [[ $(grep "Invalid hashlist!" $i.txt) ]]; then rm $i.txt; fi
-   if [[ $(grep "Hashlist is deleted!" $i.txt) ]]; then rm $i.txt; fi
+#    if [[ $(grep "Invalid hashlist!" $i.txt) ]]; then rm $i.txt; fi
+#    if [[ $(grep "Hashlist is deleted!" $i.txt) ]]; then rm $i.txt; fi
    
-   cat $i.txt >> holy_wow.txt
-   rm $i.txt
+#    cat $i.txt >> holy_wow.txt
+#    rm $i.txt
 
-done
+# done
 
 
 
@@ -319,7 +487,7 @@ sort holy_wow.txt | uniq
 
 
 # Stop timer and display
+date --rfc-2822
 timer
 
 echo "Total unique passwords in holy_wow: $(wc -l < holy_wow.txt)"
-
